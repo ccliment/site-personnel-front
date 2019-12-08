af3f6c4494b28fb7c77c0e1c2cd3b6371906feb9
Charles Climent <charlesclimentpro@gmail.com>
2019-12-08 14:38:37 +0100

    feat: suppression du formulaire de contact

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,7 +4,6 @@ import { Subject, Subscription } from 'rxjs';
 import { NgwWowService } from 'ngx-wow';
 import * as Parallax from 'parallax-js';
 import { MatDialog } from '@angular/material';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AlertService } from '../../bundles/alert-message/alert-message.service';
 import { RequestService } from '../../services/request/request.service';
 import { Scroll } from '../../class/scroll';
@@ -37,8 +36,6 @@ export class HomeComponent implements OnInit, OnDestroy, AfterContentInit {
 
   /** Mon âge, issu du calcul de ma date de naissance et de la date du jour */
   public age: number;
-  /** Formulaire de contact */
-  public contact_form: FormGroup;
   /** État du bouton de soumission du formulaire */
   public submitInProgress: boolean = false;
 
@@ -60,7 +57,6 @@ export class HomeComponent implements OnInit, OnDestroy, AfterContentInit {
     private _router: Router,
     private $wow: NgwWowService,
     private _dialog: MatDialog,
-    private _fb: FormBuilder,
     private $alert: AlertService,
     private $request: RequestService,
     public scroll: Scroll
@@ -78,23 +74,6 @@ export class HomeComponent implements OnInit, OnDestroy, AfterContentInit {
 
     const diff = new Date(today.getTime() - birthday.getTime());
     this.age = diff.getFullYear() - 1970;
-
-    this.contact_form = this._fb.group({
-      name: this._fb.control('', [
-        Validators.required
-      ]),
-      email: this._fb.control('', [
-        Validators.email,
-        Validators.required
-      ]),
-      subject: this._fb.control('', [
-        Validators.maxLength(150)
-      ]),
-      message: this._fb.control('', [
-        Validators.required,
-        Validators.maxLength(5000)
-      ])
-    });
 
     if ('loading' in HTMLImageElement.prototype) {
       document.querySelectorAll('img[loading="lazy"]').forEach((img: HTMLImageElement) => {
@@ -162,86 +141,6 @@ export class HomeComponent implements OnInit, OnDestroy, AfterContentInit {
    */
   public openDialog(index: number): void {
     this._dialog.open(this.expDesc.toArray()[index]);
-  }
-
-  /**
-   * Indication si la valeur contenue dans le champ ciblé est invalide.
-   *
-   * @param field
-   *    Champ ciblé
-   */
-  public fieldIsInvalid(field: string): boolean {
-    return this.contact_form.get(field).invalid;
-  }
-
-  /**
-   * Retourne le message d'erreur du champ ciblé.
-   *
-   * @param field
-   *    Champ ciblé
-   */
-  public getFieldErrorMessage(field: string): string | null {
-    const target = this.contact_form.get(field);
-
-    if (target.invalid) {
-      switch (field) {
-        case 'email':
-          if (target.hasError('email')) {
-            return 'Le format de l\'adresse email est invalide';
-          }
-
-          return 'L\'email est requis';
-        case 'subject':
-          if (target.hasError('maxlength')) {
-            return 'La longueur du sujet est trop grande'
-          }
-
-        break;
-        case 'message':
-          if (target.hasError('maxlength')) {
-            return 'La longueur du message est trop grande';
-          }
-
-          return 'Le contenu du message est requis';
-      }
-    }
-
-    return null;
-  }
-
-  /**
-   * Soumission du formulaire.
-   *
-   * @param event
-   *    Événement de validation.
-   *
-   * @remarks L'événement est nécessaire pour réinitialiser la validation sur les champs du formulaire.
-   */
-  public async onSubmit(event: Event) {
-    if (this.submitInProgress) {
-      return;
-    }
-
-    this.submitInProgress = true;
-
-    try {
-      await this.$request.sendContactForm(this.contact_form.value);
-
-      (event.target as HTMLFormElement).reset();
-      this.contact_form.reset();
-
-      this.$alert.openAlert({
-        success: true,
-        message: 'Votre message a correctement été transmis, merci !'
-      });
-    } catch (e) {
-      this.$alert.openAlert({
-        success: false,
-        message: 'Une erreur s\'est produite lors de la communication avec l\'API :('
-      });
-    }
-
-    this.submitInProgress = false;
   }
 
   /**
